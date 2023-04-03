5cdd9fcba836785ba5a8c51c4cb19b7b8054784c
parilbeladiya <parilbeladiya>
2023-04-03 22:28:48 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { AutoSuggest as Control } from "./controls/autoSuggest/AutoSuggest";
+import { AntDesignTextbox as Control } from "./controls/antDesignTextbox/AntDesignTextbox";
+// import { AutoSuggest as Control } from "./controls/autoSuggest/AutoSuggest";
+// import { MaterialDesignTextbox as Control } from "./controls/materialDesignTextbox/MaterialDesignTextbox";
+// import { MinimalTextbox as Control } from "./controls/minimalTextbox/MinimalTextbox";
+// import { MultiSelectDropdown as Control } from "./controls/multiSelectDropdown/MultiSelectDropdown";
+// import { Pagination as Control } from "./controls/pagination/Pagination";
+// import { RadioButtons as Control } from "./controls/radioButtons/RadioButtons";
+// import { RangeSlider as Control } from "./controls/rangeSlider/RangeSlider";
+// import { RippleButton as Control } from "./controls/rippleButton/RippleButton";
 
 function App() {
   return (
